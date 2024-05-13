f7b830735cbded93a9b549f00c31c7168a2c8b74
fire8327 <fire83274@gmail.com>
2024-05-13 22:17:22 +0300

    create header

---
+++ components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="w-full grid-container py-4 relative">
+        <div class="flex items-center justify-between gap-6">
+            <NuxtLink to="/">
+                <img src="/images/header/logo.png" alt="" class="w-16">
+            </NuxtLink>
+            <nav class="flex items-center gap-6">
+                <NuxtLink>Главная</NuxtLink>
+                <NuxtLink>Педагоги</NuxtLink>
+                <NuxtLink>О нас</NuxtLink>
+                <NuxtLink>Контакты</NuxtLink>
+                <NuxtLink class="rounded-2xl p-0.5 animate-text">
+                    <span class="px-4 py-2 rounded-xl grow">Вход</span>
+                </NuxtLink>
+            </nav>
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
