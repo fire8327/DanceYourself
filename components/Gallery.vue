f4d593d87e7b7be60f5cf9500d6bc734c2f6eeda
fire8327 <fire83274@gmail.com>
2024-05-14 20:43:48 +0300

    upd

--- components/Gallery.vue
+++ components/Gallery.vue
@@ -5,14 +5,14 @@
             <p>Галерея</p>
         </div>
         <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-6">
-            <img src="/images/gallery/1.jpg" alt="" class="rounded-xl aspect-video object-cover">
-            <img src="/images/gallery/2.jpg" alt="" class="rounded-xl aspect-video object-cover">
-            <img src="/images/gallery/3.jpg" alt="" class="rounded-xl aspect-video object-cover">
-            <img src="/images/gallery/4.jpg" alt="" class="rounded-xl aspect-video object-cover">
-            <img src="/images/gallery/5.jpg" alt="" class="rounded-xl aspect-video object-cover">
-            <img src="/images/gallery/6.jpg" alt="" class="rounded-xl aspect-video object-cover">
-            <img src="/images/gallery/7.jpg" alt="" class="rounded-xl aspect-video object-cover">
-            <img src="/images/gallery/8.jpg" alt="" class="rounded-xl aspect-video object-cover">
+            <img src="/images/gallery/1.jpg" alt="" class="rounded-xl aspect-video object-cover transition-all duration-500 hover:scale-110">
+            <img src="/images/gallery/2.jpg" alt="" class="rounded-xl aspect-video object-cover transition-all duration-500 hover:scale-110">
+            <img src="/images/gallery/3.jpg" alt="" class="rounded-xl aspect-video object-cover transition-all duration-500 hover:scale-110">
+            <img src="/images/gallery/4.jpg" alt="" class="rounded-xl aspect-video object-cover transition-all duration-500 hover:scale-110">
+            <img src="/images/gallery/5.jpg" alt="" class="rounded-xl aspect-video object-cover transition-all duration-500 hover:scale-110">
+            <img src="/images/gallery/6.jpg" alt="" class="rounded-xl aspect-video object-cover transition-all duration-500 hover:scale-110">
+            <img src="/images/gallery/7.jpg" alt="" class="rounded-xl aspect-video object-cover transition-all duration-500 hover:scale-110">
+            <img src="/images/gallery/8.jpg" alt="" class="rounded-xl aspect-video object-cover transition-all duration-500 hover:scale-110">
         </div>
     </div>
 </template>
