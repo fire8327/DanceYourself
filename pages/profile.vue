75c2e93c5e886d069b3554e1559ae642a637c86e
fire8327 <fire83274@gmail.com>
2024-05-14 13:36:39 +0300

    add logout

--- pages/profile.vue
+++ pages/profile.vue
@@ -1,9 +1,79 @@
 <template>
-    <div>
-        Профиль
+    <div class="flex flex-col gap-6">
+        <div class="flex items-center gap-4 text-3xl font-Pacifico">
+            <Icon class="text-3xl text-[#f48fb1]/70" name="material-symbols:contacts-rounded"/>
+            <p>Контактные данные</p>
+        </div>
+        <FormKit @submit="updateUser" type="form" :actions="false" messages-class="hidden" form-class="flex flex-col gap-4 items-center justify-center">
+            <div class="flex items-center lg:items-start gap-4 max-lg:flex-col md:w-2/3 lg:w-1/2">
+                <FormKit type="text" v-model="user.name" validation="required" messages-class="text-[#E9556D] font-Comfortaa" name="Фамилия" outer-class="w-full lg:w-1/3" input-class="px-4 py-2 border border-[#673ab7]/70 rounded-xl focus:outline-none w-full" placeholder="Фамилия"/>
+                <FormKit type="text" v-model="user.surname" validation="required" messages-class="text-[#E9556D] font-Comfortaa" name="Имя" outer-class="w-full lg:w-1/3" input-class="px-4 py-2 border border-[#673ab7]/70 rounded-xl focus:outline-none w-full" placeholder="Имя"/>
+                <FormKit type="text" v-model="user.patronymic" validation="required" messages-class="text-[#E9556D] font-Comfortaa" name="Отчество" outer-class="w-full lg:w-1/3" input-class="px-4 py-2 border border-[#673ab7]/70 rounded-xl focus:outline-none w-full" placeholder="Отчество"/>
+            </div>
+            <div class="flex items-center lg:items-start gap-4 max-lg:flex-col md:w-2/3 lg:w-1/2">
+                <FormKit type="text" v-model="user.login" validation="required" messages-class="text-[#E9556D] font-Comfortaa" name="Логин" outer-class="w-full lg:w-1/2" input-class="px-4 py-2 border border-[#673ab7]/70 rounded-xl focus:outline-none w-full" placeholder="Логин"/>
+                <FormKit type="text" v-model="user.password" validation="required|length:6" messages-class="text-[#E9556D] font-Comfortaa" name="Пароль" outer-class="w-full lg:w-1/2" input-class="px-4 py-2 border border-[#673ab7]/70 rounded-xl focus:outline-none w-full" placeholder="••••••"/>
+            </div>            
+            <button type="submit" class="w-[160px] text-center py-0.5 px-4 rounded-full bg-[#673ab7]/70 border border-[#673ab7]/70 text-white transition-all duration-500 hover:text-[#673ab7]/70 hover:bg-transparent">Изменить</button>
+        </FormKit>
+    </div>
+    <div class="flex flex-col gap-6">
+        <div class="flex items-center gap-4 text-3xl font-Pacifico">
+            <Icon class="text-3xl text-[#f48fb1]/70" name="material-symbols:contacts-rounded"/>
+            <p>Выход из аккаунта</p>
+        </div>
+        <button @click="logout" type="button" class="w-[160px] text-center py-0.5 px-4 rounded-full hover:bg-[#673ab7]/70 border border-[#673ab7]/70 hover:text-white transition-all duration-500 text-[#673ab7]/70 bg-transparent">Выйти</button>
     </div>
 </template>
 
 <script setup>
+    /* подключение БД и проверка пользователя */
+    const supabase = useSupabaseClient() 
+    const { authenticated, role, id } = storeToRefs(useUserStore())
 
+    const { data:users, error } = await supabase
+    .from('users')
+    .select('*')   
+    .eq('id', id.value)  
+
+
+    /* создание сообщений и роутера */
+    const showMessage = useMessagesStore().showMessage
+    const router = useRouter()
+
+
+    /* создание формы пользователя */
+    const user = ref({
+        name: users[0].name,
+        surname: users[0].surname,
+        patronymic: users[0].patronymic,
+        login: users[0].login,
+        password: users[0].password
+    }) 
+
+
+    /* обновление данных */
+    const updateUser = async () => {        
+        const { data, error } = await supabase
+        .from('users')
+        .update({ name: user.value.name, surname: user.value.surname, patronymic: user.value.patronymic, login: user.value.login, password: user.value.password})
+        .eq('id', id.value)
+          
+        if(error) {
+            console.log(error)
+            showMessage("Произошла ошибка!", false)   
+        } else {            
+            showMessage("Данные обновлены!", true)   
+        }
+    }
+
+
+    /* выход из аккаунта */
+    const logout = () => {
+        authenticated.value = false
+        id.value = null
+        role.value = ""
+        showMessage("Успешный выход!", true) 
+        router.push("/")
+    }
 </script>
